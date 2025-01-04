c7112308c027e431d2b52e4b9171eb313dcce8a5
unknown <yobiz2000@gmail.com>
2025-01-03 21:53:31 -0600

    Cambios en el reproductor de musica

--- js/music.js
+++ js/music.js
@@ -1,12 +1,7 @@
-var audio = new Audio('resources/song.mp3');
-
-audio.autoplay = true;
-audio.loop = true;
-audio.muted = true;
-
 document.body.addEventListener('click', function() {
-    audio.muted = false;
-    audio.play().catch(function(error) {
-        console.error("Error al reproducir el audio: ", error);
-    });
+    var audioController = document.getElementById("music-controller");
+
+    if (audioController != null) {
+        audioController.pause();
+    }
 });
